58faf3f83770c45d036c4d52d20da1271ebd36a4
ArtemisiaK <artemis.karozis@gmail.com>
2023-10-25 09:34:13 +1100

    added loader spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
 export default function Weather() {
   function handleResponse(response) {
@@ -8,5 +9,17 @@ export default function Weather() {
   let apiKey = "202t323f488633ba301345o8b10a7e9f";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}`;
   axios.get(apiURL).then(handleResponse);
-  return <h2>testing one two</h2>;
+  return (
+    <div>
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="red"
+        ariaLabel="three-dots-loading"
+        wrapperStyle
+        wrapperClass
+      />
+    </div>
+  );
 }
